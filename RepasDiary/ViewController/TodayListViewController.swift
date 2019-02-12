bce65cc9aa166e8e4fdac67e5cdf7bd0ddfd6599
SangWookPark <wkahdla12346@gmail.com>
2019-02-12 12:38:55 +0900

    +++Update Project Basic Setting

--- RepasDiary/ViewController/TodayListViewController.swift
+++ RepasDiary/ViewController/TodayListViewController.swift
@@ -2,19 +2,20 @@
 //  ViewController.swift
 //  RepasDiary
 //
-//  Created by 박소정 on 12/02/2019.
+//  Created by 박상욱 on 12/02/2019.
 //  Copyright © 2019 sangwook park. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodayListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
-
 }
 
+extension TodayListViewController: UICollectionViewDelegate {
+    
+}
