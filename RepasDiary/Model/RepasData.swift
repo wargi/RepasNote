6070b0a4931a97909a7f9fbe2dd22bbdbf8d6a25
SangWookPark <wkahdla12346@gmail.com>
2019-02-20 11:36:58 +0900

    +++Update Realm Model Setting

---
+++ RepasDiary/Model/RepasData.swift
@@ -0,0 +1,49 @@
+//
+//  RepasData.swift
+//  RepasDiary
+//
+//  Created by 박소정 on 20/02/2019.
+//  Copyright © 2019 sangwook park. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+// Year
+class YearRecord: Object {
+   // Property
+   @objc dynamic var year = 0
+   var monthsRecord = List<MonthRecord>()
+   
+   // Convenience init
+   convenience init(year: Int) {
+      self.init()
+      self.year = year
+   }
+}
+
+// Month
+class MonthRecord: Object {
+   // Property
+   @objc dynamic var month: Int = 0
+   var daysRecord = List<DayRecord>()
+   
+   // Convenience init
+   convenience init(month: Int) {
+      self.init()
+      self.month = month
+   }
+}
+
+// Day
+class DayRecord: Object {
+   // Property
+   @objc dynamic var breakfast = ""
+   @objc dynamic var lunch = ""
+   @objc dynamic var dinner = ""
+   
+   @objc dynamic var water = 0
+   
+   @objc dynamic var intensity = ""
+   @objc dynamic var workout = ""
+}
--- RepasDiary/ViewController/TodayListViewController.swift
+++ RepasDiary/ViewController/TodayListViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class TodayListViewController: UIViewController {
 
+    @IBOutlet private weak var collectionView : UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
+}
+
+extension TodayListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return UICollectionViewCell(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: self.view.frame.width / 3,
+                                                  height: self.view.frame.width / 3))
+    }
+    
+    
 }
 
 extension TodayListViewController: UICollectionViewDelegate {
